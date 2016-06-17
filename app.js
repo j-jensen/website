3d1b866caded1b1551f9097fbedc43f17a886227
j-jensen <jesperjensen358@gmail.com>
2016-06-17 16:09:40 +0200

    added option to bodyparser-call

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
